b387d54687a0a6064d10be6c39982e3d7351f831
Jonas Oliveira <jonascsoliveira@outlook.com>
2021-05-03 21:06:57 -0300

    first commit

---
+++ backend/src/main/java/com/jonasoliveira/appdsvendas/AppDsVendasApplication.java
@@ -0,0 +1,13 @@
+package com.jonasoliveira.appdsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDsVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDsVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jonasoliveira/appdsvendas/AppDsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonasoliveira.appdsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
